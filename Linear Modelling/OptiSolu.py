1f54e8af579ec146e4a8c8eb793ee24fec206ec3
Hy~ <31476608+Hyperfresh8080@users.noreply.github.com>
2021-02-25 21:31:25 +1030

    rename, enhance optimal solutions
    Optimal soulutions finder now supports floating point numbers

---
+++ Linear Modelling/OptiSolu.py
@@ -0,0 +1,29 @@
+# Find an optimal solution for maximising
+# or minimising the solution within a
+# feasible region.
+
+# Intended for linear programming tasks.
+
+print("Linear Programming:\nOptimal Solution\nFinder\n")
+
+def optype():
+    global optype
+    optype = input("Max + or min -? ")
+    if optype != ("+" or "-"): optype()
+optype()
+
+x = float(input("Input x const: "))
+y = float(input("Input y const: "))
+
+print("To exit, enter '0' as\nvertex points for\nx and y.")
+def repeat():
+    a = float(input("Input vertex x: "))
+    b = float(input("Input vertex y: "))
+    if optype == "+":
+        sol = round((x*a) + (y*b), 2)
+    else:
+        sol = round((x*a) - (y*b), 2)
+    print("Solution is " + str(sol) + "\n")
+    if sol == 0: return
+    else: repeat()
+repeat()
