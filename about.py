09b9db032ccf7c8e5a27f28f222a596b3ee5e0bd
Hy~ <31476608+Hyperfresh8080@users.noreply.github.com>
2021-02-25 21:31:31 +1030

    Create about.py

---
+++ about.py
@@ -0,0 +1,128 @@
+# Character print limit is 21x7.
+con = ">>> Press EXE >>>"
+
+print("""Casio Collection -
+a collection of math
+scripts for
+calculators""")
+input(con)
+print("""Casio Collection is a
+collection of Python
+scripts that run on
+the Casio fx-CG 50 AU
+calculator, meant to
+help you with""")
+input(con)
+print("""slightly boring and
+repetetitve maths.
+Because coding in
+BASIC is boring.""")
+input(con)
+print("""This is free
+software, released
+into the public
+domain as per the
+Unlicense.""")
+input(con)
+print("""Do whatever you want
+with this code, as
+far as the law
+permits.
+See the LICENSE file
+for more details.""")
+input(con)
+print("""All code in this
+collection, including
+this about file, meet
+and follow
+SonarSource rules.""")
+input(con)
+print("""See https://rules.
+sonarsource.com
+/python for more
+detail""")
+input(con)
+print("""Developed by
+http://github.com
+/hyperfresh""")
+if input("View license? (y/n) ") == "y":
+    print("""This is free and
+unencumbered software
+released into the
+public domain.""")
+    input(con)
+    print("""Anyone is free to
+copy, modify,
+publish, use,
+compile, sell, or
+distribute this
+software,""")
+    input(con)
+    print("""either in source code
+form or as a compiled
+binary, for any
+purpose, commercial
+or non-commercial,
+and by any means.""")
+    input(con)
+    print("""In jurisdictions that
+recognize copyright
+laws, the author or
+authors of this
+software dedicate any
+and all copyright""")
+    input(con)
+    print("""interest in the
+software to the
+public domain. We
+make this
+dedication for the
+benefit of the""")
+    input(con)
+    print("""public at large
+and to the
+detriment of our
+heirs and
+successors.""")
+    input(con)
+    print("""We intend this
+dedication to be an
+overt act of
+relinquishment in
+perpetuity of all
+present and future""")
+    input(con)
+    print("""rights to this
+software under
+copyright law.""")    
+    input(con)
+    print("""THE SOFTWARE IS
+PROVIDED "AS IS",
+WITHOUT WARRANTY OF
+ANY KIND, EXPRESS OR
+IMPLIED,  INCLUDING
+BUT NOT LIMITED TO """)
+    input(con)
+    print("""THE WARRANTIES OF
+MERCHANTABILITY,
+FITNESS FOR A
+PARTICULAR PURPOSE
+AND NONINFRINGEMENT.""")
+    input(con)
+    print("""IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR
+ANY CLAIM, DAMAGES OR
+OTHER LIABILITY,
+WHETHER IN AN ACTION
+OF CONTRACT, TORT OR""")
+    input(con)
+    print("""OTHERWISE, ARISING
+FROM, OUT OF OR IN
+CONNECTION WITH THE
+SOFTWARE OR THE USE
+OR OTHER DEALINGS IN
+THE SOFTWARE.""")
+    input(con)
+    print("""For more information,
+please refer to
+http://unlicense.org""")
